d70e59d715308a3ffaeddf720d416a22822145f9
Romain Deveaud <romain.deveaud@gmail.com>
2014-01-28 17:05:38 +0000

    added a PDF crawler from ACM DL, not tested

---
+++ crawl_pdfs.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""Copyright (C) 2014 Romain Deveaud <romain.deveaud@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+import re
+import urllib2
+import hashlib
+from bs4 import BeautifulSoup
+
+def get_conf_path(conf,year):
+  return os.getcwd()+'/papers/'+conf+'/'+year
+
+def create_folders(conf,year):
+  """
+  Creates the structure of folders for a new conference.
+  """
+  path = get_conf_path(conf,year)
+
+  try:
+    os.makedirs(path)
+  except OSError as exc: # Python >2.5
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else: raise
+
+def get_acm_doi_links_from_dblp(conf,year):
+  """
+  Given the acronym of a conference and a year, this method extracts 
+  the ACM DOI links of the papers from DBLP.
+
+  TODO: it could consider many other repositories by changing the
+        regex.
+  """
+
+  dblp_url = 'http://www.informatik.uni-trier.de/~ley/db/conf/'+ \
+              conf+'/'+conf+year+'.html'
+
+  dblp_parsed_html = BeautifulSoup(urllib2.urlopen(dblp_url).read())
+
+  acm_links = dblp_parsed_html.body.findAll('a',attrs={'href':re.compile('doi\.acm\.org')})
+  return [link['href'] for link in acm_links]
+
+def get_pdf_from_acm_url(url):
+  """
+  It requires an access to the ACM DL library.
+  Creates the appropriate folder if it does not exist, then download the
+  PDF of the paper corresponding to the DOI link given as argument.
+  """
+  
+  acm_parsed_html = BeautifulSoup( \
+                      urllib2.urlopen( \
+                        urllib2.Request(url, headers={'User-Agent' : "Magic Browser"}) \
+                      ).read() \
+                    )
+  pdf_link = 'http://dl.acm.org/'+acm_parsed_html.body.find('a',attrs={'name':'FullTextPDF'})['href']
+
+  pdf = urllib2.urlopen(urllib2.Request(pdf_link, headers={'User-Agent' : "Magic Browser"})).read()
+  return pdf
+
+def download_pdf(pdf_data,destination):
+  """
+  Write the PDF data at the specified destination.
+  """
+  pdf_file = open(destination,'wb')
+  pdf_file.write(pdf_data)
+  pdf_file.close()
+
+def main(argv):
+  for link in get_acm_doi_links_from_dblp(argv[1],argv[2]):
+    download_pdf(get_pdf_from_acm_url(link),pdf_path+'/'+(abs(hash(link)) % (10 ** 8))+'.pdf')
+
+
+# TODO: check the arguments, maybe with an option parser.
+if __name__ == '__main__': sys.exit(main(sys.argv))
--- extract_text.py
+++ extract_text.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python
+"""Copyright (C) 2014 Romain Deveaud <romain.deveaud@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""
+This script recursively retrieves all the PDFs stored in papers/<conf>/pdf/ and
+extract their text using the pdf2txt.py utility.
+Extracted text is stored in papers/<conf>/txt/.  
+"""
+
+# TODO: only extract the text of papers that we haven't seen so far.
+
 import sys
 import os
 import subprocess
+
 
 def main(argv):
   rootdir = os.getcwd()+'/papers'
